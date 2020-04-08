9b41485c4664836d9eecca28404c23c84c0b27b4
Jonathan Mazala <mazala.2@osu.edu>
2020-04-08 16:22:04 -0600

    added a little bit simpler solution

--- sorted-squared-array/index.js
+++ sorted-squared-array/index.js
@@ -1,5 +1,32 @@
 //https://www.youtube.com/watch?v=4eWKHLSRHPY&list=WL&index=13
-function sortedSquaredArrayPointers(nums) {
+function sortedSquaredArrayPointersWhileLoop(nums) {
+  let low = 0;
+  let high = nums.length - 1;
+  const answer = [];
+
+  while (low <= high) {
+    if (Math.abs(nums[low]) > Math.abs(nums[high])) {
+      answer.unshift(nums[low] * nums[low++]);
+      continue;
+    }
+
+    if (Math.abs(nums[high]) > Math.abs(nums[low])) {
+      answer.unshift(nums[high] * nums[high--]);
+      continue;
+    }
+
+    if (low === high) {
+      answer.unshift(nums[high] * nums[high--]);
+      continue;
+    }
+
+    answer.unshift(nums[low] * nums[low++]);
+  }
+
+  return answer;
+}
+
+function sortedSquaredArrayPointersNegPos(nums) {
   const answer = [];
 
   let positiveIndex = 0;
@@ -35,15 +62,17 @@ function sortedSquaredArrayPointers(nums) {
       answer.push(nums[positiveIndex] * nums[positiveIndex++]);
       continue;
     }
-    
+
     answer.push(nums[negativeIndex] * nums[negativeIndex--]);
   }
 }
 
 function sortedSquaredArrayCheating(nums) {
   //O(n log n) to sort, O(n) to map = O(n log n)
-  return nums.sort((a, b) => Math.abs(a) - Math.abs(b)).map(i => i*i);
+  return nums.sort((a, b) => Math.abs(a) - Math.abs(b)).map(i => i * i);
 }
 
 console.log(sortedSquaredArrayCheating([-3, -1, 0, 2, 5, 10]));
-console.log(sortedSquaredArrayPointers([-3, -1, 0, 2, 5, 10]));
+console.log(sortedSquaredArrayPointersNegPos([-3, -1, 0, 2, 5, 10]));
+console.log(sortedSquaredArrayPointersWhileLoop([-3, -1, 0, 2, 5, 10]));
+console.log(sortedSquaredArrayPointersWhileLoop([-3, -1, 2, 5, 10]));
