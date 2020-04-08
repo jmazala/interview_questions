0ea402dbfac997f710c104ea2c0d6811c479382b
Jonathan Mazala <mazala.2@osu.edu>
2020-04-07 18:24:06 -0600

    reversing a string / sentence / char array magic

---
+++ reverse-words-in-array/Solution.java
@@ -0,0 +1,43 @@
+//https://www.youtube.com/watch?v=aotBpjJUqJo&t=1800s
+
+public class Solution {
+  public static char[] reverseCharArray(char[] chars) {
+    reverseCharArray(chars, 0, chars.length - 1);
+    return chars;
+  }
+
+  public static void reverseCharArray(char[] chars, int start, int end) {
+    for (int i = 0; i < Math.ceil((start + end) / 2); i++) {
+      char temp = chars[start + i];
+      chars[start + i] = chars[end - i];
+      chars[end - i] = temp;
+    }
+  }
+
+  public static String reverseSentence(String s) {
+    return reverseString(s, 0, s.length() - 1);
+  }
+  
+  public static String reverseSentence(String s, int start, int end) {
+    StringBuilder builder = new StringBuilder();
+    for (int i = 0; i < start; i++) {
+      builder.append(s.charAt(i));
+    }
+
+    for (int i = end; i >= start; i--) {
+      builder.append(s.charAt(i));
+    }
+
+    for (int i = end; i < s.length() - 1; i++) {
+      builder.append(s.charAt(i));
+    }
+
+    return builder.toString();
+  }
+
+  public static void main(String[] args) {
+    System.out.println(Solution.reverseSentence("topaet elttil a m'I"));
+    System.out.println(Solution.reverseCharArray(new char[] {'t', 'o', 'p', 'a', 'e', 't', ' ', 'e', 'l', 't', 't', 'i', 'l', ' ', 'a', ' ', 'm', '\'', 'I'}));
+  }
+}
+
