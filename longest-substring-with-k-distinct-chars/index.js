515bc9da910f3738ed5b12ac6a8fe02ab6454392
Jonathan Mazala <mazala.2@osu.edu>
2020-05-22 09:36:29 -0700

    push / pull from array insted of copying a bunch of strings

--- longest-substring-with-k-distinct-chars/index.js
+++ longest-substring-with-k-distinct-chars/index.js
@@ -1,47 +1,41 @@
 //https://start.interviewing.io/feedback/tIE1HyPSYovX
+
+const objSize = o => Object.keys(o).length;
+
 function findLongestSubstringOfKDistinct(string, k) {
-  if (k === 0 || !string) {
+  if (k === 0 || string.length === 0) {
     return '';
   }
   
   const charCounts = {};
-  let answer = '';
-  let current = '';
-  let left = 0;
-  let right = 0;
+  const current = [];
+  let answer = [];
 
   //add the character to the hash
   //if we've exceeded k, adjust sliding window
   //delete that previous char from the hash at the end
 
-  while (right < string.length) {
-    const c = string[right];
+  for (let i = 0; i < string.length; i++) {
+    const c = string[i];
     charCounts[c] = charCounts[c] || 0;
     charCounts[c]++;
+    current.push(c);
 
     //adjust our sliding window if we've exceeded k chars
-    if (Object.keys(charCounts).length > k) {
-      if (current.length > answer.length) {
-        answer = current;
+    while (objSize(charCounts) > k) {
+      const removedChar = current.shift();
+      charCounts[removedChar]--;
+      if (charCounts[removedChar] === 0) {
+        delete charCounts[removedChar];
       }
-
-      const originalLeftChar = string[left];
-      while (charCounts[originalLeftChar] > 0) {
-        const leftChar = string[left];
-        charCounts[leftChar]--;
-        left++;
-        current = current.slice(1);
-      }
-
-      // {'b': 1, 'd': 1 }
-      delete charCounts[originalLeftChar];
     }
 
-    current += c;
-    right++;
+    if (current.length > answer.length) {
+      answer = current.slice();
+    }
   }
 
-  return answer;
+  return answer.join('');
 }
 
 //BRUTE FORCE NAIVE
@@ -78,16 +72,10 @@ function findLongestSubstringOfKDistinct(string, k) {
 //   }
 // }
 
-
-
-
-console.log(findLongestSubstringOfKDistinct('abbaaabddccdddcgge', 0));
-console.log(findLongestSubstringOfKDistinct('abbaaabddccdddcgge', 1));
-console.log(findLongestSubstringOfKDistinct('abbaaabddccdddcgge', 2));
-console.log(findLongestSubstringOfKDistinct('abbaaabddccdddcgge', 3));
-
-
-
+console.log(findLongestSubstringOfKDistinct('abbaaabddccdddcgge', 0)); // ''
+console.log(findLongestSubstringOfKDistinct('abbaaabddccdddcgge', 1)); // 'aaa' or 'ddd'
+console.log(findLongestSubstringOfKDistinct('abbaaabddccdddcgge', 2)); // 'ddccdddc'
+console.log(findLongestSubstringOfKDistinct('abbaaabddccdddcgge', 3)); // 'ddccdddcgg'
 
 /*
 
